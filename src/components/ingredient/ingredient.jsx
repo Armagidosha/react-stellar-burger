6f4475601803dd4fb22ea1f07241729f30b72bad
Vladimir Z <armgidosha@list.ru>
2023-07-07 22:12:24 +0300

    add: ingredient component

---
+++ src/components/ingredient/ingredient.jsx
@@ -0,0 +1,28 @@
+import PropTypes from 'prop-types';
+import { CurrencyIcon } from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from './ingredient.module.css';
+import { memo } from 'react';
+
+const Ingredient = memo(({data, onClick}) => {
+  return (
+    <div className={styles.container} onClick={() => onClick(data)}>
+      <img className={`${styles.image} pr-4 pl-4`} src={data.image} alt={data.name} />
+      <div className={`${styles.priceContainer} mt-1 mb-1`}>
+        <p className={'text text_type_digits-default pr-2'}>{data.price}</p>
+        <CurrencyIcon type="primary" />
+      </div>
+      <p className={'text text_type_main-default'}>{data.name}</p>
+    </div>
+  );
+});
+
+Ingredient.propTypes = {
+  data: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+  }).isRequired,
+  onClick: PropTypes.func.isRequired,
+};
+
+export default Ingredient;
