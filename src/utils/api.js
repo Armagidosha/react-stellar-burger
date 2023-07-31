30e5aba70ce5f7a7d12b8457c11e5e66d5409c0e
Vladimir Z <armgidosha@list.ru>
2023-07-31 10:27:01 +0300

    feat: add postOrderInfo function

--- src/utils/api.js
+++ src/utils/api.js
@@ -8,28 +8,63 @@ const checkResponse = (res) => {
 };
 
 export const fetchIngredients = async setData => {
-try {
-  setData(prevData => ({
-    ...prevData,
-    isLoading: true
-  }))
-  
-  const response = await fetch(`${utils.url}/ingredients`);
-  const data = await checkResponse(response);
+  try {
+    setData(prevData => ({
+      ...prevData,
+      isLoading: true
+    }))
 
-  setData(prevData => ({
-    ...prevData,
-    isLoading: false,
-    hasError: false,
-    data: data.data
-  }))
-} catch (error) {
-  console.error(`Ошибка: ${error}`);
+    const response = await fetch(`${utils.url}/ingredients`);
+    const data = await checkResponse(response);
 
-  setData(prevData => ({
-    ...prevData,
-    isLoading: false,
-    hasError: true
-  }))
+    setData(prevData => ({
+      ...prevData,
+      isLoading: false,
+      hasError: false,
+      data: data.data
+    }))
+  } catch (error) {
+    console.error(`Ошибка: ${error}`);
+
+    setData(prevData => ({
+      ...prevData,
+      isLoading: false,
+      hasError: true
+    }))
+  }
 }
+export const postOrderData = async (postData, setOrderData) => {
+  try {
+    setOrderData(prevOrderData => ({
+      ...prevOrderData,
+      isLoading: true
+    }))
+
+    const response = await fetch(`${utils.url}/orders`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+      }
+    );
+
+    const data = await checkResponse(response);
+    setOrderData(prevOrderData => ({
+      ...prevOrderData,
+      isLoading: false,
+      hasError: false,
+      data: data.order.number
+    })
+    )
+  } catch (error) {
+    console.error(`Ошибка: ${error}`);
+
+    setOrderData(prevOrderData => ({
+      ...prevOrderData,
+      isLoading: false,
+      hasError: true
+    }))
+  }
 }
