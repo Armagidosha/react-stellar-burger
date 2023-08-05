36904bcac5c8ad24d8b92c31803dd70751d08314
Vladimir Z <armgidosha@list.ru>
2023-08-05 17:35:42 +0300

    refactor: remove Modal

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -3,22 +3,17 @@ import AppHeader from "../app-header/app-header";
 import BurgerConstructor from "../burger-constructor/burger-constructor";
 import BurgerIngredients from "../burger-ingredients/burger-ingredients";
 import styles from "./app.module.css";
-import OrderDetails from "../order-details/order-details";
-import Modal from "../modal/modal";
-import IngredientDetails from "../ingredient-details/ingredient-details";
-import { fetchIngredients } from "../../utils/api";
 import { useDispatch, useSelector } from "react-redux";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
+import { getIngredients } from "../../services/actions/ingredients";
 
 function App() {
   const dispatch = useDispatch();
   const data = useSelector(store => store.ingredients);
-  const modal = useSelector(store => store.modal);
-  const order = useSelector(store => store.order)
 
   useEffect(() => {
-    dispatch(fetchIngredients())
+    dispatch(getIngredients())
   }, [dispatch]);
 
   return (
@@ -38,15 +33,6 @@ function App() {
           </span>
         )}
       </main>
-      {modal.isOpened && !order.orderRequest && (
-        <Modal>
-          {modal.currentModal === 'ingredient' ? (
-            <IngredientDetails />
-          ) : (
-            !order.orderRequest && <OrderDetails />
-          )}
-        </Modal>
-      )}
     </div>
   );
 }
