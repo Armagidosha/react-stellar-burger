ee9746a6e0ead18e95c74c1df802ab46675928b5
Vladimir Z <armgidosha@list.ru>
2023-08-06 22:12:54 +0300

    refactor: add burgerTabs component

--- src/components/burger-ingredients/burger-ingredients.jsx
+++ src/components/burger-ingredients/burger-ingredients.jsx
@@ -1,14 +1,12 @@
-import { Tab } from '@ya.praktikum/react-developer-burger-ui-components';
-import { useMemo, useState, useRef, useEffect } from 'react';
+import { useMemo, useRef } from 'react';
 import styles from './burger-ingredients.module.css';
 import Ingredient from '../ingredient/ingredient';
 import { useSelector } from 'react-redux';
 import IngredientDetails from '../ingredient-details/ingredient-details';
 import Modal from '../modal/modal';
+import { BurgerTab } from '../burger-tab-items/burger-tab-items';
 
 const BurgerIngredients = () => {
-  const [current, setCurrent] = useState('bun');
-
   const bunRef = useRef(null);
   const sauceRef = useRef(null);
   const mainRef = useRef(null);
@@ -23,64 +21,10 @@ const BurgerIngredients = () => {
     return [bun, sauce, main];
   }, [data]);
 
-  const scrollIntoIngredient = (section) => {
-    setCurrent(section);
-    const element = document.getElementById(section);
-    if (element) element.scrollIntoView({ behavior: 'smooth' });
-  };
-
-  const getDistanceFromTop = (element) => {
-    if (element) {
-      const rect = element.getBoundingClientRect();
-      return rect.top;
-    }
-    return Infinity;
-  };
-  const setActiveTabOnScroll = () => {
-    const bunDistance = Math.abs(getDistanceFromTop(bunRef.current));
-    const sauceDistance = Math.abs(getDistanceFromTop(sauceRef.current));
-    const mainDistance = Math.abs(getDistanceFromTop(mainRef.current));
-
-    if (bunDistance <= sauceDistance && bunDistance <= mainDistance) {
-      setCurrent('bun');
-    } else if (sauceDistance < bunDistance && sauceDistance < mainDistance) {
-      setCurrent('sauce');
-    } else {
-      setCurrent('main');
-    }
-  };
-
-  useEffect(() => {
-    const handleScroll = () => {
-      setActiveTabOnScroll(container.scrollTop);
-    };
-    const container = document.querySelector('.scrollContainer');
-    container.addEventListener('scroll', handleScroll);
-    return () => {
-      container.removeEventListener('scroll', handleScroll);
-    };
-  });
-
   return (
     <section className={`pt-10`}>
       <h2 className='text text_type_main-large'>Соберите бургер</h2>
-      <ul className={`${styles.tab_ul} pt-5`}>
-        <li className={styles.tab_list}>
-          <Tab value="bun" active={current === 'bun'} onClick={scrollIntoIngredient}>
-            Булки
-          </Tab>
-        </li>
-        <li className={styles.tab_list}>
-          <Tab value="sauce" active={current === 'sauce'} onClick={scrollIntoIngredient}>
-            Соусы
-          </Tab>
-        </li>
-        <li className={styles.tab_list}>
-          <Tab value="main" active={current === 'main'} onClick={scrollIntoIngredient}>
-            Начинки
-          </Tab>
-        </li>
-      </ul>
+      <BurgerTab bunRef={bunRef} sauceRef={sauceRef} mainRef={mainRef}/>
       <div className={`${styles.ingredients_container} pt-10 custom-scroll scrollContainer`}>
         <h2 ref={bunRef} id="bun" className={`text text_type_main-medium pb-6`}>
           Булки
