116070e5b373091417e7db34951b1f45f1ecca51
Vladimir Z <armgidosha@list.ru>
2023-09-08 02:24:11 +0300

    refactor: add useCallback

--- src/utils/hooks.js
+++ src/utils/hooks.js
@@ -7,6 +7,6 @@ export const useInput = (event) => {
   const handleChange = useCallback((event) => {
     const {value, name} = event.target;
     setValues({...inputs, [name]: value});
-  });
+  }, [inputs]);
   return {inputs, handleChange};
 }
