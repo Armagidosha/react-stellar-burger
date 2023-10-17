4084e4fc0d82fb3887932ff89d2e457d60bbcd60
Armagidoshka <armgidosha@list.ru>
2023-10-17 16:57:50 +0300

    fix: ws typing

--- src/utils/webSocket-middleware.ts
+++ src/utils/webSocket-middleware.ts
@@ -1,11 +1,14 @@
 import { AppDispatch, RootState } from "..";
 import { wsActions } from "../types/types";
-import { refreshToken } from "./api";
 import { Middleware, MiddlewareAPI } from "@reduxjs/toolkit";
+import { refreshToken } from "./api";
+import { utils } from "./utils";
 
 export const socketMiddleware = (wsActions: wsActions): Middleware => {
   return (store: MiddlewareAPI<AppDispatch, RootState>) => {
     let socket: WebSocket | null = null;
+    let errorMessage = ''
+    let timer: ReturnType<typeof setTimeout>
 
     return next => action => {
       const { dispatch } = store;
@@ -19,7 +22,7 @@ export const socketMiddleware = (wsActions: wsActions): Middleware => {
         wsDisconnect,
       } = wsActions;
 
-      if (wsConnect.match(action)) {
+      if (wsConnect.match(action) && !socket) {
         socket = new WebSocket(action.payload || '');
         dispatch(wsConnecting());
       }
@@ -29,42 +32,53 @@ export const socketMiddleware = (wsActions: wsActions): Middleware => {
           dispatch(onOpen());
         };
 
-        socket.onerror = (event) => {
+        socket.onerror = () => {
           dispatch(onError('Error'));
-          console.error(event)
         };
 
         socket.onmessage = async (event: MessageEvent) => {
           const { data } = event;
           const parsedData = JSON.parse(data);
           if (parsedData.message === 'Invalid or missing token') {
-            await refreshToken()
+            errorMessage = parsedData.message
           } else {
             if (socket) {
               dispatch(onMessage({
                 data: parsedData,
                 currentUrl: socket.url
-              })
-              );
+              }));
             }
           }
         };
 
-        socket.onclose = (event) => {
-          dispatch(onClose());
+        socket.onclose = async (event) => {
+          dispatch(onClose())
           if (event.code === 1006) {
-            setTimeout(() => {
+            if (errorMessage === 'Invalid or missing token') {
+              const { accessToken } = await refreshToken()
               if (socket) {
-                const currentUrl = socket.url
-                dispatch(wsConnect(currentUrl))
+                dispatch(wsDisconnect())
               }
-            }, 5000);
+              dispatch(wsConnect(`${utils.orders}?token=${accessToken.split('Bearer ')[1]}`))
+              errorMessage = ''
+            } else {
+              dispatch(wsDisconnect())
+              timer = setTimeout(() => {
+                if (!socket) {
+                  dispatch(wsConnect((event.target as WebSocket).url))
+                }
+              }, 5000)
+            }
+          } else {
+            dispatch(wsDisconnect())
           }
         };
 
         if (wsDisconnect.match(action)) {
           socket.close();
           socket = null;
+          errorMessage = ''
+          clearTimeout(timer)
         }
       }
 
